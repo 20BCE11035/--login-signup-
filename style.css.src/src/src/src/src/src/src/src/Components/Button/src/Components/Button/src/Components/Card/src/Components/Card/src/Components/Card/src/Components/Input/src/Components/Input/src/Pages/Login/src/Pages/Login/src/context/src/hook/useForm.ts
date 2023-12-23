820e756536fbae02a482a646f2f6821246dfe113
ANANT KUMAR <153983455+20BCE11035@users.noreply.github.com>
2023-12-23 15:41:11 +0530

    Create useForm.ts

---
+++ style.css.src/src/src/src/src/src/src/src/Components/Button/src/Components/Button/src/Components/Card/src/Components/Card/src/Components/Card/src/Components/Input/src/Components/Input/src/Pages/Login/src/Pages/Login/src/context/src/hook/useForm.ts
@@ -0,0 +1,43 @@
+import { ChangeEvent, FormEvent, useState } from "react"
+
+type InitailValuesTypes<IT> = IT
+type errorsValuesTypes = { [key: string]: string }
+export default function useForm<IT extends { [key: string]: string | number | null }>(initailValues: InitailValuesTypes<IT>,
+    errorsValues: errorsValuesTypes) {
+    const [formValues, setformValues] = useState<IT>(initailValues)
+    const [errors, seterrors] = useState<errorsValuesTypes>(errorsValues)
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>, validtype = true, validationType = true) => {
+        setformValues({
+            ...formValues, [e.target.name]: e.target.value ? validtype ? e.target.value :
+                formValues[e.target.name] : ""
+        })
+        if (e.target.value) {
+            if (validationType) {
+                delete errors[`${e.target.name}_error`]
+                seterrors({ ...errors })
+            }else{
+                seterrors({ ...errors, [`${e.target.name}_error`]: `${e.target.name.replace(/_/g, " ")} is not Valid` })
+            }
+        } else {
+            if (e.target.required) {
+                seterrors({ ...errors, [`${e.target.name}_error`]: `${e.target.name.replace(/_/g, " ")} is required` })
+            }else{
+                delete errors[`${e.target.name}_error`]
+                seterrors({ ...errors })
+            }
+        }
+    }
+
+    const onSubmit = (e: FormEvent, submit: (formValue: IT) => void) => {
+        e.preventDefault()
+        if (Object.entries(errors).length === 0) {
+            submit(formValues)
+        } else {
+            Object.keys(errors).map(res => errors[res] = `${(res.substring(0, res.length - 5)).replace(/_/g, " ")} is required`)
+            seterrors({ ...errors })
+        }
+    }
+
+    return { formValues, handleInputChange, errors, onSubmit }
+}
